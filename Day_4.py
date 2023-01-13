ae86c47e231e7cdc361c67ba3888e9ff4992ceaa
AlexanderLevkin <alexanlevkin@gmail.com>
2023-01-13 20:11:02 +0300

    100 days of code course

---
+++ Day_4.py
@@ -0,0 +1,9 @@
+# import random
+#
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+#
+# random_float = round(random.random(), 5)
+# print(random_float)
+
+states_of_america = ["Delaware", "Pennsylvania"]
