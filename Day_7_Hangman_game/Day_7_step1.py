9a35b2e9b6143d7d4da1af3a510fb164fb352230
AlexanderLevkin <alexanlevkin@gmail.com>
2023-01-17 20:50:52 +0300

    100 days of code course

---
+++ Day_7_Hangman_game/Day_7_step1.py
@@ -0,0 +1,20 @@
+# Step 1
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = input("Guess a latter: ").lower()
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for latter in chosen_word:
+    if guess == latter:
+        print("Right")
+    else:
+        print("Wrong")
---
+++ Day_7_Hangman_game/Day_7_step2.py
@@ -0,0 +1,32 @@
+#Step 2
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+# TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+letters_in_word = int(len(chosen_word))
+display = ["_"]*letters_in_word
+print(display)
+
+guess = input("Guess a letter: ").lower()
+
+# TODO-2: - Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+g = 0
+for letter in chosen_word:
+    g += 1
+    if letter == guess:
+        display.pop(g)
+        display.insert(g-1, guess)
+print(display)
+
+# TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
