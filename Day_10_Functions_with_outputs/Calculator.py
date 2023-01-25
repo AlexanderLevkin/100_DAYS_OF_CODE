56102a32081a91cc60975ac68ea71b13eced3139
AlexanderLevkin <alexanlevkin@gmail.com>
2023-01-25 07:48:31 +0300

    add Day 10 completed

--- Day_10_Functions_with_outputs/Calculator.py
+++ Day_10_Functions_with_outputs/Calculator.py
@@ -1,6 +1,7 @@
 # Calculator
-
+from art import logo
 # Add
+
 def add(n1, n2):
     return n1 + n2
 
@@ -27,19 +28,28 @@ operators = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for operator in operators:
-    print(operator)
-operation_symbol = input("Pick an operator from the line above: ")
-calculation_function = operators[operation_symbol]
-num2 = int(input("What's the second number?: "))
-first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+def calculation():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for operator in operators:
+        print(operator)
 
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the second number?: "))
-calculation_function = operators[operation_symbol]
-second_answer = calculation_function(calculation_function(num1, num2), num3)
+    switcher = True
+    while switcher:
 
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        operation_symbol = input("Pick an operator: ")
+        calculation_function = operators[operation_symbol]
+        num2 = float(input("What's the next number?: "))
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        wish = input("Do you want to continue? press y to continue n - to start again:")
+        if wish == "y":
+            num1 = answer
+        else:
+            switcher = False
+            calculation()
+
+
+calculation()
