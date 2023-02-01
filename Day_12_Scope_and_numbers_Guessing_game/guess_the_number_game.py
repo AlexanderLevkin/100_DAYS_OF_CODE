be38922e949458ffbe7ffe3fdac5aa6711738f8b
AlexanderLevkin <alexanlevkin@gmail.com>
2023-02-01 18:18:29 +0300

    add completed guess number game and day 13

--- Day_12_Scope_and_numbers_Guessing_game/guess_the_number_game.py
+++ Day_12_Scope_and_numbers_Guessing_game/guess_the_number_game.py
@@ -1,44 +1,87 @@
-# Number Guessing Game Objectives:
-import random
 
-# Include an ASCII art logo.
+from random import randint
 from logo import art
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
-# Allow the player to submit a guess for a number between 1 and 100.
-NUMBER = random.randint(1, 100)
+#Function to check user's guess against actual answer.
+def check_answer(guess, answer, turns):
+  """checks answer against guess. Returns the number of turns remaining."""
+  if guess > answer:
+    print("Too high.")
+    return turns - 1
+  elif guess < answer:
+    print("Too low.")
+    return turns - 1
+  else:
+    print(f"You got it! The answer was {answer}.")
 
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+#Make function to set difficulty.
+def set_difficulty():
+  level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  if level == "easy":
+    return EASY_LEVEL_TURNS
+  else:
+    return HARD_LEVEL_TURNS
 
-user_input = input(f"Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nPssst, "
-                   f"the correct answer is "
-                   f"{NUMBER}\nChoose a difficulty. Type 'easy' or 'hard': \n").lower()
+def game():
+  print(art)
+  #Choosing a random number between 1 and 100.
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+  answer = randint(1, 100)
+  print(f"Pssst, the correct answer is {answer}")
 
+  turns = set_difficulty()
+  #Repeat the guessing functionality if they get it wrong.
+  guess = 0
+  while guess != answer:
+    print(f"You have {turns} attempts remaining to guess the number.")
 
-if user_input == "easy":
-    attempts = 10
-    print(f"You have {attempts} attempts remaining to guess the number.")
-elif user_input == "hard":
-    attempts = 5
-    print(f"You have {attempts} attempts remaining to guess the number.")
-else:
-    print("You type wrong symbols")
-
-while attempts > 0:
+    #Let the user guess a number.
     guess = int(input("Make a guess: "))
-    attempts -= 1
-    if guess == NUMBER:
-        print("YOU WON!!!.")
-        attempts = 0
-    elif guess > NUMBER:
-        print("Too hight.")
-    elif guess < NUMBER:
-        print("Too low.")
-    print(f"You have {attempts} attempts remaining to guess the number.")
 
-else:
-    print("You've run out of guesses, you lose.")
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player.
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+    #Track the number of turns and reduce by 1 if they get it wrong.
+    turns = check_answer(guess, answer, turns)
+    if turns == 0:
+      print("You've run out of guesses, you lose.")
+      return
+    elif guess != answer:
+      print("Guess again.")
+
+
+game()
+
+
+# NUMBER = random.randint(1, 100)
+#
+# user_input = input(f"Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nPssst, "
+#                    f"the correct answer is "
+#                    f"{NUMBER}\nChoose a difficulty. Type 'easy' or 'hard': \n").lower()
+#
+#
+# if user_input == "easy":
+#     attempts = 10
+#     print(f"You have {attempts} attempts remaining to guess the number.")
+# elif user_input == "hard":
+#     attempts = 5
+#     print(f"You have {attempts} attempts remaining to guess the number.")
+# else:
+#     print("You type wrong symbols")
+#
+# while attempts > 0:
+#     guess = int(input("Make a guess: "))
+#     attempts -= 1
+#     if guess == NUMBER:
+#         print("YOU WON!!!.")
+#         attempts = 0
+#     elif guess > NUMBER:
+#         print("Too hight.")
+#     elif guess < NUMBER:
+#         print("Too low.")
+#     print(f"You have {attempts} attempts remaining to guess the number.")
+#
+# else:
+#     print("You've run out of guesses, you lose.")
+#
