525ecd0e36ac5fd0fcd898e18c77f699dd212451
AlexanderLevkin <levkin.alexan@gmail.com>
2023-02-18 18:39:26 +0300

    add completed day 21

--- Day_20_Build_snake_Game_Part_1/main.py
+++ Day_20_Build_snake_Game_Part_1/main.py
@@ -1,6 +1,8 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Score
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Score()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,5 +25,24 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    # Detect collusion with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
+
+    # Detect collusion with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+    # If head collides with any segment in the tail:
+        # trigger game_over
 
 screen.exitonclick()
