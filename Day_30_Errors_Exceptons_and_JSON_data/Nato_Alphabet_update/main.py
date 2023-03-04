4cea2ec9f538a670b842cb6e88ab5f00db65c3ee
levkin.a <levkin.alexan@gmail.com>
2023-03-04 12:30:05 +0300

    add Day 30 almost cpmpleted

--- Day_30_Errors_Exceptons_and_JSON_data/Nato_Alphabet_update/main.py
+++ Day_30_Errors_Exceptons_and_JSON_data/Nato_Alphabet_update/main.py
@@ -1,15 +1,24 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def nato_alph():
+    phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+    print(phonetic_dict)
+
+    word = input("Enter a word: ").upper()
+    output_list = [phonetic_dict[letter] for letter in word]
+    print(output_list)
+
+
+var_catch = True
+
+while var_catch:
+    try:
+        nato_alph()
+        var_catch = False
+
+    except KeyError:
+        print("Sorry, only letters in alphabet please!")
+
