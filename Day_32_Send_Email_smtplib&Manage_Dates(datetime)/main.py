1d3f5dd360a59557af2239d3346b1dc8a306ca60
AlexanderLevkin <levkin.alexan@gmail.com>
2023-03-12 21:22:41 +0300

    add Day 32 completed

--- Day_32_Send_Email_smtplib&Manage_Dates(datetime)/main.py
+++ Day_32_Send_Email_smtplib&Manage_Dates(datetime)/main.py
@@ -1,8 +1,9 @@
 import smtplib
+import random
 
-# my_email = "arterial888@gmail.com"
-# recipient_email = "alexanderlevkin@yahoo.com"
-# password = "odqvzefbnzuoocaa"
+my_email = "arterial888@gmail.com"
+recipient_email = "alexanderlevkin@yahoo.com"
+password = "odqvzefbnzuoocaa"
 #
 # with smtplib.SMTP("smtp.gmail.com") as connection:
 #     connection.starttls()
@@ -13,11 +14,25 @@ import smtplib
 import datetime as dt
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
+# year = now.year
+# month = now.month
+# print(day_of_the_week)
+# print(year)
+#
+# date_of_birth = dt.datetime(year=1991, month=12, day=15, hour=4)
+# print(date_of_birth)
+
+# ---------------------------------------IF DAY IS SAT THE EMAIL SEND----------------------------#
+with open("quotes.txt") as file:
+    list_of_quotes = [quote for quote in file.readlines()]
+
 day_of_the_week = now.weekday()
-print(day_of_the_week)
-print(year)
 
-day
-
+if day_of_the_week == 5:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
+                            msg=random.choice(list_of_quotes))
+else:
+    print("This is not Monday")
