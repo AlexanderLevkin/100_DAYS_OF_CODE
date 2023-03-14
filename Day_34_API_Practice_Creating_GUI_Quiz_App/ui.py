9ae1fc576eca8926c149357fb1d2430c9bc9d255
levkin.a <levkin.alexan@gmail.com>
2023-03-14 19:18:48 +0300

    add day 34 completed

--- Day_34_API_Practice_Creating_GUI_Quiz_App/ui.py
+++ Day_34_API_Practice_Creating_GUI_Quiz_App/ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -31,13 +33,10 @@ class QuizInterface:
         self.false_button.grid(row=2, column=0)
         self.true_button.grid(row=2, column=1)
 
-
-
-        self.false_button = Button()
-
-
-
-
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
