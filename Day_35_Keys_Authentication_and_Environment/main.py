7c64180054b5a9d5cff9203cadd5aa8f4e63a7b8
AlexanderLevkin <levkin.alexan@gmail.com>
2023-03-14 23:32:28 +0300

    refactor

--- Day_35_Keys_Authentication_and_Environment/main.py
+++ Day_35_Keys_Authentication_and_Environment/main.py
@@ -1,15 +1,15 @@
 import requests
 
-api_key = "0564af452ff5dc7be4147cedd3b6284d"
+api_key = "f86e6a4afd3eb7c5fc434f4e601e9efe"
 
-endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
 parameters = {
-    "lat": 55.532051,
-    "lon": 28.660320,
+    "lat": 55.53,
+    "lon": 28.66,
     "appid": api_key,
 }
 
-response = requests.get(url=endpoint, params="parameters")
+response = requests.get(url=endpoint, params=parameters)
 response.raise_for_status()
 print(response.json())
