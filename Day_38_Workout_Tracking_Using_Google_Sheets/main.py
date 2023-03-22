4d82ac067d0880b222705a73404dbf2d2606f66d
levkin.a <levkin.alexan@gmail.com>
2023-03-22 19:16:45 +0300

    add day 38 almost completed

--- Day_38_Workout_Tracking_Using_Google_Sheets/main.py
+++ Day_38_Workout_Tracking_Using_Google_Sheets/main.py
@@ -1,7 +1,19 @@
+from datetime import datetime
+
 import requests
 import pprint
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/ba78c813be217d53517d29d03421df29/workoutTracking/workouts"
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+customer_choice = input("Tell me which the ex you did: ")
+GENDER = "male"
+WEIGHT_KG = 78
+HEIGHT_CM = 182
+AGE = 31
 
 APP_ID = "de21c256"
 API_KEY = "5ceae0e6fa43755d33894d0ade9890bd"
@@ -13,13 +25,15 @@ HEADERS = {
 }
 
 body = {
-    "query": "ran 3 miles",
-    "gender": "female",
-    "weight_kg": 72.5,
-    "height_cm": 167.64,
-    "age": 30
+    "query": customer_choice,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
 }
 
 response = requests.post(url=exercise_endpoint, json=body, headers=HEADERS)
 print(response.text)
 
+response_of_sheety_get = requests.get(sheety_endpoint)
+print(response_of_sheety_get.text)
